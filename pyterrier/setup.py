4eee88a595bc7cfdcc09cdd869cf9a86967fb0f4
Aleksandar Tsolov <2267643t@student.gla.ac.uk>
2019-12-09 02:29:39 +0000

    Renamed main.py to pyterrier.py

--- pyterrier/setup.py
+++ pyterrier/setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyterrier", # Replace with your own username
-    version="0.0.4",
+    name="python-terrier",
+    version="0.0.2",
     author="A-Tsolov",
     author_email="tsolov.aleksandar@gmail.com",
     description="Terrier IR Python API",
@@ -13,12 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/A-Tsolov/Pyterrier",
     packages=setuptools.find_packages(),
-    py_modules=["main"],
+    py_modules=["pyterrier","batchretrieve", "utils"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    data_files=[('my_data', ['terrier-project-5.1-jar-with-dependencies.jar'])],
+    # data_files=[('my_data', ['terrier-project-5.1-jar-with-dependencies.jar'])],
     python_requires='>=3.6',
 )
