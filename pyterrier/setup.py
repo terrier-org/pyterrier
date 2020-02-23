1af9254fe7f7b268b89a1f123ebf43183e60634f
Aleksandar Tsolov <2267643t@student.gla.ac.uk>
2020-02-23 23:47:05 +0000

    Added archived vaswani index

--- pyterrier/setup.py
+++ pyterrier/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-terrier",
-    version="0.0.14",
+    version="0.1.0",
     author="A-Tsolov",
     author_email="tsolov.aleksandar@gmail.com",
     description="Terrier IR Python API",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/A-Tsolov/Pyterrier",
     packages=setuptools.find_packages(),
-    py_modules=["pyterrier","batchretrieve", "utils"],
+    py_modules=["pypyterrier","batchretrieve", "utils", "index"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
