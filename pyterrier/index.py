c5553a79a1274de6e217555d9735ff94b83f3547
Aleksandar Tsolov <2267643t@student.gla.ac.uk>
2020-03-23 23:32:06 +0000

    Added index docstrings

--- pyterrier/index.py
+++ pyterrier/index.py
@@ -1,3 +1,7 @@
+"""
+This file contains all the indexers.
+"""
+
 from jnius import autoclass, cast, PythonJavaClass, java_method
 from utils import *
 import pandas as pd
@@ -21,9 +25,19 @@ Properties = autoclass('java.util.Properties')
 CLITool = autoclass("org.terrier.applications.CLITool")
 
 class Indexer:
-    '''
-    Parent class. Use one of its children classes
-    '''
+    """
+    Parent class. It can be used to load an existing index.
+    Use one of its children classes if you wish to create a new index.
+
+    Attributes:
+        default_properties(dict): Contains the default properties
+        path(str): The index directory + \data.properties
+        index_called(bool): True if index() method of child Indexer has been called, false otherwise
+        index_dir(str): The index directory
+        blocks(bool): If true the index has blocks enabled
+        properties: A Terrier Properties object, which is a hashtable with properties and their values
+        overwrite(bool): If True the index() method of child Indexer will overwrite any existing index
+    """
 
     default_properties={
             "TrecDocTags.doctag":"DOC",
@@ -33,6 +47,14 @@ class Indexer:
             "trec.collection.class": "TRECCollection",
     }
     def __init__(self, index_path, blocks=False, overwrite=False):
+        """
+        Init method
+
+        Args:
+            index_path (str): Directory to store index
+            blocks (bool): Create indexer with blocks if true, else without blocks
+            overwrite (bool): If index already present at `index_path`, True would overwrite it, False throws an Exception
+        """
         self.path = os.path.join(index_path, "data.properties")
         self.index_called = False
         self.index_dir = index_path
@@ -44,10 +66,24 @@ class Indexer:
             os.makedirs(index_path)
 
     def setProperties(self, **kwargs):
+        """
+        Set the properties to the given ones
+
+        Usage:
+            setProperties("property1=value1, property2=value2")
+            or
+            setProperties("**{property1:value1, property2:value2}")
+
+        Args:
+            **kwargs: Properties to set to
+        """
         for control,value in kwargs.items():
             self.properties.put(control,value)
 
     def checkIndexExists(self):
+        """
+        Check if index exists at the `path` given when object was created
+        """
         if os.path.isfile(self.path):
             if not self.overwrite :
                 raise ValueError("Index already exists at " + self.path)
@@ -55,6 +91,9 @@ class Indexer:
             raise Exception("Index method can be called only once")
 
     def createIndexer(self):
+        """
+        Check `blocks` and create a BlockIndexer if true, else create BasicIndexer
+        """
         ApplicationSetup.bootstrapInitialisation(self.properties)
         if self.blocks:
             index = BlockIndexer(self.index_dir,"data")
@@ -63,39 +102,76 @@ class Indexer:
         return index
 
     def createAsList(self, files_path):
+        """
+        Helper method to be used by child indexers to add files to Java List
+        """
         if type(files_path) == type(""):
             asList = Arrays.asList(files_path)
-        if type(files_path) == type([]):
+        else if type(files_path) == type([]):
             asList = Arrays.asList(*files_path)
         return asList
 
     def getIndexStats(self):
+        """
+        Returns the index statistics
+
+        Note:
+            Does not work with notebooks at the moment
+        """
         CLITool.main(["indexstats", "-I" + self.path])
 
 
     def getIndexUtil(self, util):
-        ''' Utilities for displaying the content of an index
-        Parameters:
-        util(string): possible values:
-        printbitentry
-        printlex
-        printlist
-        printlistentry
-        printmeta
-        printposting
-        printpostingfile
-        printterm
-        s
-        '''
+        """
+        Utilities for displaying the content of an index
+
+        Note:
+            Does not work with notebooks at the moment
+
+        Args:
+            util: which util to print
+
+        Possible Utils:
+            util(string): possible values:
+            printbitentry
+            printlex
+            printlist
+            printlistentry
+            printmeta
+            printposting
+            printpostingfile
+            printterm
+            s
+        """
         if not util.startswith("-"):
             util = "-"+util
         CLITool.main(["indexutil", "-I" + self.path, util])
 
 class DFIndexer(Indexer):
-    '''
-    Use for Pandas dataframe
-    '''
+    """
+    Use this Indexer if you wish to index a pandas.Dataframe
+
+    Attributes:
+        default_properties(dict): Contains the default properties
+        path(str): The index directory + \data.properties
+        index_called(bool): True if index() method of child Indexer has been called, false otherwise
+        index_dir(str): The index directory
+        blocks(bool): If true the index has blocks enabled
+        properties: A Terrier Properties object, which is a hashtable with properties and their values
+        overwrite(bool): If True the index() method of child Indexer will overwrite any existing index
+    """
     def index(self, text, *args, **kwargs):
+        """
+        Index the specified
+
+        Args:
+            text(pd.Series): A pandas.Series(a column) where each row is the body of text for each document
+            *args: Either a pandas.Dataframe or pandas.Series.
+                If a Dataframe: All columns(including text) will be passed as metadata
+                If a Series: The Series name will be the name of the metadata field and the body will be the metadata content
+            **kwargs: Either a list, a tuple or a pandas.Series
+                The name of the keyword argument will be the name of the metadata field and the keyword argument contents will be the metadata content
+        """
         self.checkIndexExists()
         all_metadata={}
         for arg in args:
@@ -135,10 +211,25 @@ class DFIndexer(Indexer):
         self.index_called=True
 
 class TRECCollectionIndexer(Indexer):
-    '''
-    Use for TREC formatted collection
-    '''
+    """
+    Use this Indexer if you wish to index a TREC formatted collection
+
+    Attributes:
+        default_properties(dict): Contains the default properties
+        path(str): The index directory + \data.properties
+        index_called(bool): True if index() method of child Indexer has been called, false otherwise
+        index_dir(str): The index directory
+        blocks(bool): If true the index has blocks enabled
+        properties: A Terrier Properties object, which is a hashtable with properties and their values
+        overwrite(bool): If True the index() method of child Indexer will overwrite any existing index
+    """
     def index(self, files_path):
+        """
+        Index the specified TREC formatted files
+
+        Args:
+            files_path: can be a String of the path or a list of Strings of the paths for multiple files
+        """
         self.checkIndexExists()
         index = self.createIndexer()
         asList = self.createAsList(files_path)
@@ -148,9 +239,24 @@ class TRECCollectionIndexer(Indexer):
 
 class FilesIndexer(Indexer):
     '''
-    Use for pdf, docx, txt etc files
+    Use this Indexer if you wish to index a pdf, docx, txt etc files
+
+    Attributes:
+        default_properties(dict): Contains the default properties
+        path(str): The index directory + \data.properties
+        index_called(bool): True if index() method of child Indexer has been called, false otherwise
+        index_dir(str): The index directory
+        blocks(bool): If true the index has blocks enabled
+        properties: A Terrier Properties object, which is a hashtable with properties and their values
+        overwrite(bool): If True the index() method of child Indexer will overwrite any existing index
     '''
     def index(self, files_path):
+        """
+        Index the specified TREC formatted files
+
+        Args:
+            files_path: can be a String of the path or a list of Strings of the paths for multiple files
+        """
         self.checkIndexExists()
         index = self.createIndexer()
         asList = self.createAsList(files_path)
