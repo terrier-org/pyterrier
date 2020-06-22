ec6f01b2b644c74df6cfb09b41bc0475552c7b47
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-06-22 21:06:47 +0100

    fix typo in output

--- tests/base.py
+++ tests/base.py
@@ -7,7 +7,7 @@ class BaseTestCase(unittest.TestCase):
         super(BaseTestCase, self).__init__(*args, **kwargs)
         terrier_version = os.environ.get("TERRIER_VERSION", None)
         if terrier_version is not None:
-            print("Testing with Terrier version" + terrier_version)
+            print("Testing with Terrier version " + terrier_version)
         if not pt.started():
             pt.init(version=terrier_version)
         self.here = os.path.dirname(os.path.realpath(__file__))
