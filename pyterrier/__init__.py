6b4897e0912b6afbb43a8b02938f00ab45ddd98c
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-06-22 18:54:42 +0100

    change to dataset default imports into pt space

--- pyterrier/__init__.py
+++ pyterrier/__init__.py
@@ -75,7 +75,7 @@ def init(version=None, mem=None, packages=[], jvm_opts=[], redirect_io=True, log
 
     from .batchretrieve import BatchRetrieve, FeaturesBatchRetrieve
     from .utils import Utils
-    from .datasets import get_dataset, list_datasets, datasets
+    from .datasets import get_dataset, list_datasets
     from .index import Indexer, FilesIndexer, TRECCollectionIndexer, DFIndexer, DFIndexUtils, IndexingType
     from .pipelines import LTR_pipeline, XGBoostLTR_pipeline
 
@@ -113,10 +113,9 @@ def init(version=None, mem=None, packages=[], jvm_opts=[], redirect_io=True, log
     _logging(logging)
     setup_jnius()
 
-    globals()["datasets"] = get_dataset
+    globals()["get_dataset"] = get_dataset
     globals()["list_datasets"] = list_datasets
-    globals()["datasets"] = datasets
-
+    
     globals()["BatchRetrieve"] = BatchRetrieve
     globals()["Indexer"] = Indexer
     globals()["FeaturesBatchRetrieve"] = FeaturesBatchRetrieve
