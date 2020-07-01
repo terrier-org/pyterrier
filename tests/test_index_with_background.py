fcdd5671d66648f628d1f861f9b1e46f4de386b3
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-07-01 18:50:03 +0100

    lots of testing

--- tests/test_index_with_background.py
+++ tests/test_index_with_background.py
@@ -17,9 +17,18 @@ class TestBackground(BaseTestCase):
         for p in direct.getPostings(index.getDocumentIndex().getDocumentEntry(docid)):
             rtr[lexicon.getLexiconEntry(p.getId()).getKey()] = p.getFrequency()
         return rtr
+    
+    def test2_manualM(self):
+        self._test2_manual(pt.index.IndexingType.MEMORY)
+    def test2_manualC(self):
+        self._test2_manual(pt.index.IndexingType.CLASSIC)
+    def test2_manualS(self):
+        self._test2_manual(pt.index.IndexingType.SINGLEPASS)
+                
 
-    def test2_manual(self):
+    def _test2_manual(self, type):
         import pyterrier as pt
+        #pt.logging("INFO")
         import pandas as pd
         df1 = pd.DataFrame({
             'docno': ['1048'],
@@ -28,9 +37,11 @@ class TestBackground(BaseTestCase):
                 'detailed analysis of h  f  noise sources in tropical ground flashes '+
                 'v  l  f  phase characteristics deduced from atmospheric waveforms']
         })
-        pd_indexer1 = pt.DFIndexer(None, type=pt.index.IndexingType.MEMORY)
+        pd_indexer1 = pt.DFIndexer(tempfile.mkdtemp(), type=type)
         indexref1 = pd_indexer1.index(df1["body"], df1["docno"])
         index1 = pt.IndexFactory.of(indexref1)
+        
+        has_direct1 = index1.hasIndexStructure("direct")
 
         indexref_big = pt.get_dataset("vaswani").get_index()
         index_big = pt.IndexFactory.of(indexref_big)
@@ -42,29 +53,50 @@ class TestBackground(BaseTestCase):
         q = "MATHEMATICAL ANALYSIS AND DESIGN DETAILS OF WAVEGUIDE FED MICROWAVE RADIATIONS"
         self.assertEqual("1048", index_big.getMetaIndex().getItem("docno", 1047))
         contents_big = TestBackground.get_contents(1047, index_big)
-        
-        for index_small in [index1, pt.autoclass("org.terrier.python.IndexWithBackground")(index1, index_big)]:
 
-            contents1 = TestBackground.get_contents(0, index_small)
-            self.assertEqual(contents1, contents_big)
+        def _check_index(index_small):
+            if has_direct1:
+                contents1 = TestBackground.get_contents(0, index_small)
+                self.assertEqual(contents1, contents_big)
 
             inv1 = index_small.getInvertedIndex()
+            print(inv1.getClass().getName())
             lex1 = index_small.getLexicon()
             for t in contents_big:
                 pointer = lex1[t]
+                print(pointer.toString())
                 p = inv1.getPostings(pointer)
+                print(p.getClass().getName())
                 rtr = p.next()
                 self.assertEqual(0, rtr)
+                self.assertEqual(p.getDocumentLength(), index_big.getDocumentIndex().getDocumentLength(1047))
                 self.assertEqual(contents_big[t], p.getFrequency())
+                self.assertEqual(p.next(), p.EOL)
 
-            br1 = pt.BatchRetrieve(index_small, wmodel="Tf")
-            brall = pt.BatchRetrieve(index_big, wmodel="Tf")
-            with_doc = pd.DataFrame([["q1", q, "1048", 1047]], columns=["qid", "query", "docno", "docid"])
-            rtr1 = br1.transform(q)
+            from jnius import JavaException
+            try:
+                br1 = pt.BatchRetrieve(index_small, wmodel="Tf")
+                brall = pt.BatchRetrieve(index_big, wmodel="Tf")
+                with_doc = pd.DataFrame([["q1", q, "1048", 1047]], columns=["qid", "query", "docno", "docid"])
+                rtr1 = br1.transform(q)
+            except JavaException as ja:
+                print(ja.stacktrace)
+                raise ja
             rtrall = brall(with_doc)            
             self.assertTrue(np.array_equal(rtr1["score"].values, rtrall["score"].values))
+        
+        _check_index(index1)
+        _check_index( pt.autoclass("org.terrier.python.IndexWithBackground")(index1, index_big))
 
-    def test_it(self):
+    def test_itM(self):
+        self._test_it(pt.index.IndexingType.MEMORY)
+    def test_itC(self):
+        self._test_it(pt.index.IndexingType.CLASSIC)
+    def test_itS(self):
+        self._test_it(pt.index.IndexingType.SINGLEPASS)
+        
+
+    def _test_it(self, type):
         import pyterrier as pt
         import pandas as pd
         df1 = pd.DataFrame({
@@ -76,7 +108,7 @@ class TestBackground(BaseTestCase):
                  'The wave were crash on the shore; it was a',
                  'The body may perhaps compensates for the loss']
         })
-        pd_indexer1 = pt.DFIndexer(None, type=pt.index.IndexingType.MEMORY)
+        pd_indexer1 = pt.DFIndexer(tempfile.mkdtemp(), type=type)
         indexref1 = pd_indexer1.index(df1["text"], df1["docno"])
 
         df2 = pd.DataFrame({
@@ -87,8 +119,8 @@ class TestBackground(BaseTestCase):
         from jnius import JavaException
         try:
 
-            pd_indexer2 = pt.DFIndexer(None, type=pt.index.IndexingType.MEMORY)
-            indexref2 = pd_indexer1.index(df2["text"], df2["docno"])
+            pd_indexer2 = pt.DFIndexer(tempfile.mkdtemp(), type=type)
+            indexref2 = pd_indexer2.index(df2["text"], df2["docno"])
             
             index1 = pt.IndexFactory.of(indexref1)
             self.assertEqual(3, index1.getCollectionStatistics().getNumberOfDocuments())
