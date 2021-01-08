2ac59d5b2e9cc34b72dca4e343623ce2b04d2034
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-01-08 15:18:46 +0000

    add setup

--- docs/conf.py
+++ docs/conf.py
@@ -18,14 +18,13 @@ sys.path.insert(0, os.path.abspath('..'))
 sys.path.append('.')
 import sphinx_rtd_theme
 
-
-
 # -- Dataset table listing -----------------------------------------------------
 import pyterrier as pt
 import textwrap
 pt.init()
 
 from extras import generate_includes
+generate_includes.setup()
 generate_includes.dataset_include()
 generate_includes.experiment_includes()
 
