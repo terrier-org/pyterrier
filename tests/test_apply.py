06b4be6af0dd09d8a6f40957ee390d5bfd934ffc
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-01-17 20:46:21 +0000

    use push_queries

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -12,12 +12,15 @@ class TestCache(BaseTestCase):
 
     def test_query_apply(self):
         stops=set(["and", "the"])
+        origquery="the bear and the wolf"
         p = pt.apply.query(
                 lambda q : " ".join([t for t in q["query"].split(" ") if not t in stops ])
             )
-        testDF = pd.DataFrame([["q1", "the bear and the wolf"]], columns=["qid", "query"])
+        testDF = pd.DataFrame([["q1", origquery]], columns=["qid", "query"])
         rtr = p(testDF)
+        print(rtr)
         self.assertEqual(rtr.iloc[0]["query"], "bear wolf")
+        self.assertEqual(rtr.iloc[0]["query_0"], origquery)
 
     def test_docscore_apply(self):
         p = pt.apply.doc_score(lambda doc_row: len(doc_row["text"]))
