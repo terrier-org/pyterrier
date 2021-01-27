96cb8e2382d6ec5f14fd6ae563f5703cff97909f
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-01-27 13:49:14 +0000

    added unit test for pt.apply.by_query

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -19,6 +19,19 @@ class TestCache(BaseTestCase):
         rtr = p(testDF)
         self.assertEqual(rtr.iloc[0]["query"], "bear wolf")
 
+    def test_by_query_apply(self):
+        inputDf = pt.new.ranked_documents([[1], [2]], qid=["1", "2"])
+        def _inc_score(res):
+            res = res.copy()
+            res["score"] = res["score"] + int(res.iloc[0]["qid"])
+            return res
+        p = pt.apply.by_query(_inc_score)
+        outputDf = p(inputDf)
+        self.assertEqual(outputDf.iloc[0]["qid"], "1")
+        self.assertEqual(outputDf.iloc[0]["score"], 2)
+        self.assertEqual(outputDf.iloc[1]["qid"], "2")
+        self.assertEqual(outputDf.iloc[1]["score"], 4)
+
     def test_docscore_apply(self):
         p = pt.apply.doc_score(lambda doc_row: len(doc_row["text"]))
         testDF = pd.DataFrame([["q1", "hello", "d1", "aa"]], columns=["qid", "query", "docno", "text"])
