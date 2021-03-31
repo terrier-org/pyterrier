1c323bdbb0ef6ad37279a2e399eea8c99b5331ed
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-03-31 13:40:19 +0100

    addresses #128 concerned pt.new.ranked_documents()

--- tests/test_new.py
+++ tests/test_new.py
@@ -13,7 +13,7 @@ class TestModel(BaseTestCase):
         self.assertEqual(1, df.iloc[0]["score"])
         self.assertEqual(pt.model.FIRST_RANK, df.iloc[0]["rank"])
 
-        df = pt.new.ranked_documents([[1]], docno=["d100"])
+        df = pt.new.ranked_documents([[1]], docno=[["d100"]])
         self.assertEqual(1, len(df))
         self.assertTrue("qid" in df.columns)
         self.assertEqual("1", df.iloc[0]["qid"])
@@ -21,6 +21,17 @@ class TestModel(BaseTestCase):
         self.assertEqual(1, df.iloc[0]["score"])
         self.assertEqual(pt.model.FIRST_RANK, df.iloc[0]["rank"])
 
+        df = pt.new.ranked_documents([[2,1], [2,1], [2,1]])
+        self.assertEqual(6, len(df))
+        qids=["a", "b", "c"]
+        df = pt.new.ranked_documents([[2,1], [2,1], [2,1]], qid=qids)
+        self.assertEqual(6, len(df))
+        offset=0
+        for qid in qids:
+            for i in [0,1]:
+                self.assertEqual(df.iloc[offset]["qid"], qid)
+                offset +=1
+ 
     def test_newR1_with_qid(self):
         df = pt.new.ranked_documents([[1]], qid=["q1"])
         self.assertEqual(1, len(df))
@@ -30,7 +41,7 @@ class TestModel(BaseTestCase):
         self.assertEqual(1, df.iloc[0]["score"])
         self.assertEqual(pt.model.FIRST_RANK, df.iloc[0]["rank"])
 
-        df = pt.new.ranked_documents([[1]], docno=["d100"], qid=["q1"])
+        df = pt.new.ranked_documents([[1]], docno=[["d100"]], qid=["q1"])
         self.assertEqual(1, len(df))
         self.assertTrue("qid" in df.columns)
         self.assertEqual("q1", df.iloc[0]["qid"])
