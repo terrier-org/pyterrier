5c662b38200375f462a2d8dab451d097ed0b2916
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-04-10 17:27:45 +0100

    improve/fix error message

--- pyterrier/io.py
+++ pyterrier/io.py
@@ -127,10 +127,8 @@ def write_results(res, filename, format="trec", **kwargs):
         * "minimal": output columns are $qid $docno $rank, tab-separated. This is used for submissions to the MSMARCO leaderboard.
     
     """
-    if format is None:
-        format = "trec" 
     if not format in SUPPORTED_RESULTS_FORMATS:
-        raise ValueError("Format %s not known, supported types are %s" % str(SUPPORTED_RESULTS_FORMATS.keys()))
+        raise ValueError("Format %s not known, supported types are %s" % (format, str(SUPPORTED_RESULTS_FORMATS.keys())))
     # convert generators to results 
     res = coerce_dataframe(res)
     return SUPPORTED_RESULTS_FORMATS[format][1](res, filename, **kwargs)
