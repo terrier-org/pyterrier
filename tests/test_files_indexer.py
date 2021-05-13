e62de4e19da07049522086848107ce1506208b18
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2021-05-13 17:31:23 +0100

    FileIndexer - metadata support from non-HTML documents #160

--- tests/test_files_indexer.py
+++ tests/test_files_indexer.py
@@ -15,19 +15,27 @@ class TestFilesIndexer(BaseTestCase):
         index = pt.IndexFactory.of(indexref)
         self.assertEqual(2, index.getCollectionStatistics().getNumberOfDocuments())
 
-    def test_2_docs_title_meta(self):
+    def test_2_docs_title_body_meta(self):
         sample_dir = os.path.join(self.here, "fixtures", "sample_docs")
         files = pt.io.find_files(sample_dir)
-        indexer = pt.FilesIndexer(self.test_dir, meta={"docno" : 20, "filename" : 512, "title" : 20}, meta_tags={"title":"title"})
+        indexer = pt.FilesIndexer(
+            self.test_dir, 
+            meta={"docno" : 20, "filename" : 512, "title" : 20, "body" : 40}, 
+            meta_tags={"title":"title", "body" : "ELSE"})
         indexref = indexer.index(files)
         index = pt.IndexFactory.of(indexref)
         
         # check index size
         self.assertEqual(2, index.getCollectionStatistics().getNumberOfDocuments())
         print(index.getMetaIndex().getAllItems(0))
+        print(index.getMetaIndex().getAllItems(1))
 
+        # determine file locations as docids
         html_file = os.path.join(sample_dir, "a.html")
         html_pos = files.index(html_file)
+        txt_file = os.path.join(sample_dir, "b.txt")
+        txt_pos = files.index(txt_file)        
+
         self.assertTrue(html_pos < len(files))
         # test filename -> docid lookup
         self.assertEqual(html_pos, index.getMetaIndex().getDocument("filename", html_file))
@@ -36,11 +44,18 @@ class TestFilesIndexer(BaseTestCase):
         # test title has been recorded in metaindex
         self.assertEqual("test title", index.getMetaIndex().getItem("title", html_pos))
 
+        from pyterrier import check_version
+        if not check_version("5.5"):
+            return
+
+        # test bodies have been recorded in metaindex
+        self.assertEqual("test body", index.getMetaIndex().getItem("body", html_pos))
+        self.assertEqual("empty text document",index.getMetaIndex().getItem("body", txt_pos))
 
     def setUp(self):
-            # Create a temporary directory
-            self.test_dir = tempfile.mkdtemp()
-            print("Created " + self.test_dir)
+        # Create a temporary directory
+        self.test_dir = tempfile.mkdtemp()
+        print("Created " + self.test_dir)
 
     def tearDown(self):
         # Remove the directory after the test
