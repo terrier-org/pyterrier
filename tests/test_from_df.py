d3d868db89eb3fb035e80b8552e88a8e15be8b90
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-04-01 15:36:37 +0100

    pass through extra columns, addresses #287

--- tests/test_from_df.py
+++ tests/test_from_df.py
@@ -13,3 +13,13 @@ class TestFromDf(BaseTestCase):
         self.assertTrue("docno" in rtr.columns)
         self.assertEqual(2, len(rtr))
 
+    def test_query_cols(self):
+        queries = pt.new.queries(["this is a query"], query_embs=[[1,1]])
+        self.assertTrue("query_embs" in queries.columns)
+        docs = pt.new.ranked_documents([[1, 2]], qid=["1"])
+        rtr = pt.transformer.SourceTransformer(docs).transform(queries)
+        self.assertTrue("query" in rtr.columns)
+        self.assertTrue("score" in rtr.columns)
+        self.assertTrue("docno" in rtr.columns)
+        self.assertTrue("query_embs" in rtr.columns)
+        self.assertEqual(2, len(rtr))
