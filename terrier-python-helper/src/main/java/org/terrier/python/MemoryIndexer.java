a6c64eb9b15f9ea00b954c4576f39b3bd15cde4a
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-09-18 11:28:27 -0700

    bump for terrier 5.7 snapshot

--- terrier-python-helper/src/main/java/org/terrier/python/MemoryIndexer.java
+++ terrier-python-helper/src/main/java/org/terrier/python/MemoryIndexer.java
@@ -28,6 +28,8 @@ package org.terrier.python;
 
 import gnu.trove.TIntHashSet;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import org.terrier.indexing.Collection;
 import org.terrier.indexing.Document;
@@ -124,81 +126,76 @@ public class MemoryIndexer extends Indexer {
 
 	/** FIXME */
     private int numberOfDocuments;
+
+	@Override
+	public void indexDocuments(Iterator<Map.Entry<Map<String,String>,DocumentPostingList>> iterDocs)
+	{
+		while(iterDocs.hasNext()) {
+			Map.Entry<java.util.Map<String,String>,DocumentPostingList> d = iterDocs.next();
+			if (d == null)
+				continue;
+			try {
+				memIndex.indexDocument(d.getKey(), d.getValue());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
     
-    public void index(Collection[] collections) {
-        this.createDirectIndex(collections);
+	@Override
+    public void index(Collection collection) {
+        this.createDirectIndex(collection);
 		this.createInvertedIndex();
     }
 	
 	/** FIXME */
-	public void createDirectIndex(Collection[] collections) {
-		final int collections_length = collections.length;
-		final boolean boundaryDocsEnabled = BUILDER_BOUNDARY_DOCUMENTS.size() > 0;
-		boolean stopIndexing = false;
+	public void createDirectIndex(Collection collection) {
 		numFields = FieldScore.FIELDS_COUNT;
 		
-		for (int collectionNo = 0; !stopIndexing
-				&& collectionNo < collections_length; collectionNo++) {
-			final Collection collection = collections[collectionNo];
-			long startCollection = System.currentTimeMillis();
-			boolean notLastDoc = false;
-			while ((notLastDoc = collection.nextDocument())) {
-				Document doc = collection.getDocument();
-				if (doc == null)
-					continue;
-				numberOfDocuments++;
-				createDocumentPostings();
-				String term;
-				while (!doc.endOfDocument()) {
-					if ((term = doc.getNextTerm()) != null && !term.equals("")) {
-						termFields = doc.getFields();
-						pipeline_first.processTerm(term);
-						
-					}
-					if (MAX_TOKENS_IN_DOCUMENT > 0
-							&& numOfTokensInDocument > MAX_TOKENS_IN_DOCUMENT)
-						break;
+		long startCollection = System.currentTimeMillis();
+		boolean notLastDoc = false;
+		while ((notLastDoc = collection.nextDocument())) {
+			Document doc = collection.getDocument();
+			if (doc == null)
+				continue;
+			numberOfDocuments++;
+			createDocumentPostings();
+			String term;
+			while (!doc.endOfDocument()) {
+				if ((term = doc.getNextTerm()) != null && !term.equals("")) {
+					termFields = doc.getFields();
+					pipeline_first.processTerm(term);
+					
 				}
-				while (!doc.endOfDocument())
-					doc.getNextTerm();
-				pipeline_first.reset();
-				try {
-					memIndex.indexDocument(doc.getAllProperties(), termsInDocument);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				if (MAX_DOCS_PER_BUILDER > 0
-						&& numberOfDocuments >= MAX_DOCS_PER_BUILDER) {
-					stopIndexing = true;
+				if (MAX_TOKENS_IN_DOCUMENT > 0
+						&& numOfTokensInDocument > MAX_TOKENS_IN_DOCUMENT)
 					break;
-				}
-				if (boundaryDocsEnabled
-						&& BUILDER_BOUNDARY_DOCUMENTS.contains(doc
-								.getProperty("docno"))) {
-					logger.warn("Document "
-							+ doc.getProperty("docno")
-							+ " is a builder boundary document. Boundary forced.");
-					stopIndexing = true;
-					break;
-				}
 			}
-			if (!notLastDoc) {
-				try {
-					collection.close();
-				} catch (IOException e) {
-					logger.warn("Couldnt close collection", e);
-				}
+			while (!doc.endOfDocument())
+				doc.getNextTerm();
+			pipeline_first.reset();
+			try {
+				memIndex.indexDocument(doc.getAllProperties(), termsInDocument);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			long endCollection = System.currentTimeMillis();
-			long secs = ((endCollection - startCollection) / 1000);
-			logger.info("Collection #" + collectionNo + " took " + secs
-					+ " seconds to index " + "(" + numberOfDocuments
-					+ " documents)");
-			if (secs > 3600)
-				logger.info("Rate: "
-						+ ((double) numberOfDocuments / ((double) secs / 3600.0d))
-						+ " docs/hour");
 		}
+		if (!notLastDoc) {
+			try {
+				collection.close();
+			} catch (IOException e) {
+				logger.warn("Couldnt close collection", e);
+			}
+		}
+		long endCollection = System.currentTimeMillis();
+		long secs = ((endCollection - startCollection) / 1000);
+		logger.info("Collection took " + secs
+				+ " seconds to index " + "(" + numberOfDocuments
+				+ " documents)");
+		if (secs > 3600)
+			logger.info("Rate: "
+					+ ((double) numberOfDocuments / ((double) secs / 3600.0d))
+					+ " docs/hour");
 
 	}
 
