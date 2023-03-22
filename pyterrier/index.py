3c2730536bc49254b686114bbcba39e47803ff88
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2023-03-22 11:41:16 +0000

    addressed feedback to prevent storing of stopwords in the enum

--- pyterrier/index.py
+++ pyterrier/index.py
@@ -331,15 +331,14 @@ class TerrierStopwords(Enum):
     def _to_obj(this):
         if isinstance(this, list):
             rtr = TerrierStopwords('custom')
-            rtr.stopwords = list(this)
-            return rtr
+            return rtr, list(this)
         try:
-            return TerrierStopwords(this)
+            return TerrierStopwords(this), None
         except ValueError:
-            return this
+            return this, None
         
     @staticmethod
-    def _indexing_config(this, termpipelines : List[str], properties : Dict[str,str], hooks : List):
+    def _indexing_config(this, stopword_list : Union[List[str], None], termpipelines : List[str], properties : Dict[str,str], hooks : List):
         if this is None or this == TerrierStopwords.none:
             pass
         if this == TerrierStopwords.terrier:
@@ -347,7 +346,9 @@ class TerrierStopwords(Enum):
         if this == TerrierStopwords.custom:
             from . import check_version
             assert check_version("5.8"), "Terrier 5.8 required"
-            properties["pyterrier.stopwords"]  = ",".join(this.stopwords)
+            assert stopword_list is not None, "expected to receive a stopword list"
+
+            properties["pyterrier.stopwords"]  = ",".join(stopword_list)
             termpipelines.append('org.terrier.python.PyTerrierCustomStopwordList$Indexing')
 
             # this hook updates the index's properties to handle the python stopwords list
@@ -355,7 +356,7 @@ class TerrierStopwords(Enum):
                 from . import cast
                 pindex = cast("org.terrier.structures.PropertiesIndex", index)
                 # store the stopwords into the Index's properties
-                pindex.setIndexProperty("pyterrier.stopwords", ",".join(this.stopwords))
+                pindex.setIndexProperty("pyterrier.stopwords", ",".join(stopword_list))
 
                 # change the stopwords list implementation: the Indexing variant obtains
                 # stopwords from the global ApplicationSetup properties, while the 
@@ -452,7 +453,7 @@ class TerrierIndexer:
         self.blocks = blocks
         self.type = type
         self.stemmer = TerrierStemmer._to_obj(stemmer)
-        self.stopwords = TerrierStopwords._to_obj(stopwords)
+        self.stopwords, self.stopword_list = TerrierStopwords._to_obj(stopwords)
         self.tokeniser = TerrierTokeniser._to_obj(tokeniser)
         self.properties = Properties()
         self.setProperties(**self.default_properties)
@@ -537,7 +538,7 @@ class TerrierIndexer:
         else:
             
             termpipeline = []
-            TerrierStopwords._indexing_config(self.stopwords, termpipeline, self.properties, self.cleanup_hooks)
+            TerrierStopwords._indexing_config(self.stopwords, self.stopword_list, termpipeline, self.properties, self.cleanup_hooks)
 
             stemmer_clz = TerrierStemmer._to_class(self.stemmer)
             if stemmer_clz is not None:
