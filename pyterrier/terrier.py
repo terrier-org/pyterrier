afca740b80e941a062f5bc0f91556a009e8917d1
Sean MacAvaney <sean.macavaney@gmail.com>
2024-04-21 12:58:42 +0100

    TerrierIndex improvements

--- pyterrier/terrier.py
+++ pyterrier/terrier.py
@@ -3,12 +3,12 @@ import pyterrier as pt
 class TerrierIndex(pt.Artifact):
     def __init__(self, path):
         self.path = path
-        self.index_ref = pt.IndexRef.of(path)
+        self._cache = {}
 
-    def retriever(self, controls=None, properties=None, metadata=["docno"],  num_results=None, wmodel=None, threads=1):
-        return pt.TerrierRetrieve(self.index_ref, controls, properties, metadata, num_results, wmodel)
+    def retriever(self, *, controls=None, properties=None, metadata=["docno"],  num_results=None, wmodel=None, threads=1):
+        return pt.TerrierRetrieve(self, controls, properties, metadata, num_results, wmodel)
 
-    def bm25(self, num_results=1000, threads=1):
+    def bm25(self, *, num_results=1000, threads=1):
         return self.retriever(wmodel='BM25', num_results=num_results, threads=threads)
 
     @classmethod
@@ -18,3 +18,8 @@ class TerrierIndex(pt.Artifact):
 
     def __repr__(self):
         return f'TerrierIndex({pt.artifact.path_repr(self.path)})'
+
+    def get_index_ref(self):
+        if 'index_ref' not in self._cache:
+            self._cache['index_ref'] = pt.IndexRef.of(self.path)
+        return self._cache['index_ref']
