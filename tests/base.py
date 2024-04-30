34672ca7977527871ca6e9c775fd80fde8f89e76
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2024-04-30 12:05:55 +0100

    make unit testing less chatty

--- tests/base.py
+++ tests/base.py
@@ -10,15 +10,20 @@ class BaseTestCase(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(BaseTestCase, self).__init__(*args, **kwargs)
         terrier_version = os.environ.get("TERRIER_VERSION", None)
-        if terrier_version is not None:
-            print("Testing with Terrier version " + terrier_version)
         terrier_helper_version = os.environ.get("TERRIER_HELPER_VERSION", None)
         if not pt.started():
+
+            # display for debugging what is being used
+            if terrier_version is not None:
+                print("Testing with Terrier version " + terrier_version)
             if terrier_helper_version is not None:
                 print("Testing with Terrier Helper version " + terrier_helper_version)
+            
             pt.init(version=terrier_version, logging="DEBUG", helper_version=terrier_helper_version)
             # jvm_opts=['-ea'] can be added here to ensure that all Java assertions are met
         self.here = os.path.dirname(os.path.realpath(__file__))
+
+        # check that pt.init() is saving its arguments
         assert "version" in pt.init_args
         assert pt.init_args["version"] == terrier_version
 
