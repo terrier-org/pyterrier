bb35f2b3397f3a06b79799c151f6ffb0bd450998
Sean MacAvaney <sean.macavaney@gmail.com>
2024-07-29 16:30:37 +0100

    update pt.model for pt.java

--- pyterrier/model.py
+++ pyterrier/model.py
@@ -1,3 +1,5 @@
+import re
+import math
 import numpy as np
 import pandas as pd
 from typing import List, Sequence
@@ -68,7 +70,6 @@ def query_columns(df : pd.DataFrame, qid=True) -> Sequence[str]:
         rtr.append("qid")
     if "query" in columns:
         rtr.append("query")
-    import re
     query_col_re = re.compile('^query_[\\d]+')
     for c in columns:
         if query_col_re.search(c):
@@ -222,8 +223,6 @@ def split_df(df : pd.DataFrame, N) -> List[pd.DataFrame]:
         type = "D"
     else:
         raise ValueError("Dataframe is not of type D,Q,R")
-    
-    from math import ceil
 
     def chunks(df, n):
         """Yield successive n-sized chunks from df."""
@@ -231,13 +230,13 @@ def split_df(df : pd.DataFrame, N) -> List[pd.DataFrame]:
             yield df.iloc[ i: min(len(df),i + n)]
     
     if type == "Q" or type == "D":         
-        splits = list( chunks(df, ceil(len(df)/N)))
+        splits = list( chunks(df, math.ceil(len(df)/N)))
         return splits
 
     rtr = []
     grouper = df.groupby("qid")
     this_group = []
-    chunk_size = ceil(len(grouper)/N)
+    chunk_size = math.ceil(len(grouper)/N)
     for qid, group in grouper:
         this_group.append(group)
         if len(this_group) == chunk_size:
