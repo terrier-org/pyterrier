60e265e39cef950e6821fc0c96201a3d4ef6da4a
Sean MacAvaney <sean.macavaney@gmail.com>
2024-07-29 13:01:30 +0100

    also disable anserini.java._post_init if not installed

--- pyterrier/anserini/java.py
+++ pyterrier/anserini/java.py
@@ -28,6 +28,10 @@ def _pre_init(jnius_config):
 
 
 def _post_init(jnius):
+    if not is_installed():
+        # pyserini not installed, do nothing
+        return
+
     # Temporarily disable the configure_classpath while pyserini is init'd, otherwise it will try to reconfigure jnius
     import pyserini.setup
     _configure_classpath = pyserini.setup.configure_classpath
