c5e9d032e9d4087a139572d0b44e8db1b01f896c
Maik Fröbe <maik.froebe@uni-jena.de>
2024-08-12 15:11:30 +0200

    Add unit tests for non-rm3 query expansion methods to ensure that fixing #407 will not change their behaviour

--- tests/test_rewrite_rm3.py
+++ tests/test_rewrite_rm3.py
@@ -1,10 +1,6 @@
 import pandas as pd
-import unittest
 import pyterrier as pt
 import os
-import shutil
-import tempfile
-import pyterrier.transformer as ptt;
 from matchpy import *
 from .base import TempDirTestCase
 import pytest
@@ -16,6 +12,7 @@ if not pt.started():
     TERRIER_PRF_ON_CLASSPATH = True
 else:
     TERRIER_PRF_ON_CLASSPATH = False
+
 
 class TestRewriteRm3(TempDirTestCase):
     """This is a set of unit tests for RM3 that can currently not run in the complete test suite, as the "com.github.terrierteam:terrier-prf:-SNAPSHOT" would have to be added to the boot classpath.
@@ -47,6 +44,70 @@ class TestRewriteRm3(TempDirTestCase):
         queriesIn = pd.DataFrame([["1", "compact"]], columns=["qid", "query"])
 
         qe = pt.rewrite.RM3(indexref)
+        br = pt.BatchRetrieve(indexref, wmodel='BM25')
+
+        actual = qe.transform(br.transform(queriesIn))
+
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(expected, actual.iloc[0]["query"])
+
+    @pytest.mark.skipif(not TERRIER_PRF_ON_CLASSPATH, reason="This test only works in isolation when terrier-prf is on the jnius classpath.")
+    def test_axiomatic_qe_expansion_for_query_compact_on_bm25(self):
+        # just ensure that AxiomaticQE results do not change
+        expected = 'applypipeline:off compact^1.000000000'
+        
+        indexref = pt.datasets.get_dataset("vaswani").get_index()
+        queriesIn = pd.DataFrame([["1", "compact"]], columns=["qid", "query"])
+
+        qe = pt.rewrite.AxiomaticQE(indexref)
+        br = pt.BatchRetrieve(indexref, wmodel='BM25')
+
+        actual = qe.transform(br.transform(queriesIn))
+
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(expected, actual.iloc[0]["query"])
+
+    @pytest.mark.skipif(not TERRIER_PRF_ON_CLASSPATH, reason="This test only works in isolation when terrier-prf is on the jnius classpath.")
+    def test_kl_qe_expansion_for_query_compact_on_bm25(self):
+        # just ensure that KLQueryExpansion results do not change
+        expected = 'applypipeline:off compact^1.840895333 design^0.348370740 equip^0.000000000 purpos^0.000000000 instrument^0.000000000 ferrit^0.000000000 anod^0.000000000 aircraft^0.000000000 microwav^0.000000000 sideband^0.000000000'
+        
+        indexref = pt.datasets.get_dataset("vaswani").get_index()
+        queriesIn = pd.DataFrame([["1", "compact"]], columns=["qid", "query"])
+
+        qe = pt.rewrite.KLQueryExpansion(indexref)
+        br = pt.BatchRetrieve(indexref, wmodel='BM25')
+
+        actual = qe.transform(br.transform(queriesIn))
+
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(expected, actual.iloc[0]["query"])
+
+    @pytest.mark.skipif(not TERRIER_PRF_ON_CLASSPATH, reason="This test only works in isolation when terrier-prf is on the jnius classpath.")
+    def test_bo1_qe_expansion_for_query_compact_on_bm25(self):
+        # just ensure that Bo1QueryExpansion results do not change
+        expected = 'applypipeline:off compact^1.822309726 design^0.287992096 equip^0.000000000 purpos^0.000000000 instrument^0.000000000 ferrit^0.000000000 anod^0.000000000 aircraft^0.000000000 microwav^0.000000000 sideband^0.000000000'
+        
+        indexref = pt.datasets.get_dataset("vaswani").get_index()
+        queriesIn = pd.DataFrame([["1", "compact"]], columns=["qid", "query"])
+
+        qe = pt.rewrite.Bo1QueryExpansion(indexref)
+        br = pt.BatchRetrieve(indexref, wmodel='BM25')
+
+        actual = qe.transform(br.transform(queriesIn))
+
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(expected, actual.iloc[0]["query"])
+
+    @pytest.mark.skipif(not TERRIER_PRF_ON_CLASSPATH, reason="This test only works in isolation when terrier-prf is on the jnius classpath.")
+    def test_dfr_qe_expansion_for_query_compact_on_bm25(self):
+        # just ensure that DFRQueryExpansion results do not change
+        expected = 'applypipeline:off compact^1.822309726 design^0.287992096 equip^0.000000000 purpos^0.000000000 instrument^0.000000000 ferrit^0.000000000 anod^0.000000000 aircraft^0.000000000 microwav^0.000000000 sideband^0.000000000'
+        
+        indexref = pt.datasets.get_dataset("vaswani").get_index()
+        queriesIn = pd.DataFrame([["1", "compact"]], columns=["qid", "query"])
+
+        qe = pt.rewrite.DFRQueryExpansion(indexref)
         br = pt.BatchRetrieve(indexref, wmodel='BM25')
 
         actual = qe.transform(br.transform(queriesIn))
