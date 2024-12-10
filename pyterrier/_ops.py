bc2265445f0c48f415fa2aa0bda0a006fbed4c16
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2024-12-10 15:34:37 +0000

    fix to fuse_right compilation; fix typo in comment

--- pyterrier/_ops.py
+++ pyterrier/_ops.py
@@ -381,7 +381,7 @@ class Compose(NAryTransformerBase):
 
     def compile(self, verbose: bool = False) -> Transformer:
         """Returns a new transformer that iteratively fuses adjacent transformers to form a more efficient pipeline."""
-        # compile constituent transformers (flatten allows complie() to return Compose pipelines)
+        # compile constituent transformers (flatten allows compile() to return Compose pipelines)
         inp = deque(_flatten((t.compile() for t in self._transformers), Compose))
         out : deque = deque()
         counter = 1
@@ -396,8 +396,6 @@ class Compose(NAryTransformerBase):
                 out.pop()
                 # add the fused pipeline to the start of the input queue so it will be processed next (must be done in reverse due to how extendleft works)
                 inp.extendleft(reversed(_flatten([fused], Compose)))
-                inp[:0] = _flatten([fused], Compose)
-                # inp = deque(_flatten([fused], Compose)) + inp
             elif out and isinstance(right, SupportsFuseLeft) and (fused := right.fuse_left(out[-1])) is not None:
                 if verbose:
                     print(f"  fuse_left {out[-1]} >> {right}  == {fused}")
