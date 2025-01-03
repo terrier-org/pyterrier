9fc61cef9380986a5a1b8070e7daaacd85686355
Sean MacAvaney <sean.macavaney@gmail.com>
2025-01-03 17:55:20 -0600

    refactoring

--- pyterrier/terrier/_index.py
+++ pyterrier/terrier/_index.py
@@ -1,7 +1,16 @@
 import os
-from typing import List, Dict
+from enum import Enum
+from typing import List, Dict, Any, Optional, Union, Literal
+from pathlib import Path
 import pyterrier as pt
 
+
+class Model(Enum):
+    bm25 = 'bm25'
+    dph = 'dph'
+
+
+_IN_MEM_PATH_PLACEHOLDER = '__IGNORE__'
 
 class TerrierIndex(pt.Artifact):
     """A Terrier index."""
@@ -10,36 +19,40 @@ class TerrierIndex(pt.Artifact):
     ARTIFACT_FORMAT = 'terrier'
     ARTIFACT_PACKAGE_HINT = 'python-terrier'
 
-    def __init__(self, path):
+    def __init__(self, path, *, _index_ref=None, _index_obj=None):
         """Initialises a TerrierIndex for the given path."""
         super().__init__(path)
-        self._index_ref = None
-        self._index_obj = None
+        if _index_ref is not None:
+            assert path == _IN_MEM_PATH_PLACEHOLDER and _index_obj is None
+        self._index_ref = _index_ref
+        if _index_obj is not None:
+            assert path == _IN_MEM_PATH_PLACEHOLDER and _index_ref is None
+        self._index_obj = _index_obj
 
     def retriever(
         self,
-        wmodel: str,
+        model: Union[Model, str],
+        model_args: Optional[Dict[str, Any]] = None,
         *,
-        controls: Dict = None,
-        properties: Dict = None,
-        metadata: List[str] = ["docno"],
         num_results: int = 1000,
+        include_fields: Optional[List[str]] = None,
         threads: int = 1,
+        verbose: bool = False,
     ) -> pt.Transformer:
-        """Creates a ``pt.terrier.Retriever`` object for this index.
+        """Creates a retriever transformer for this index.
 
         Args:
-            wmodel: The weighting model to use for scoring.
-            controls: The controls to set for this retriever. Controls are specific settings for a given search request.
-            properties: The properties to use for this retriever. Properties are settings that apply globally to the index.
-            metadata: The metadata fields to return for each search result.
+            model: The weighting model to use for scoring.
+            model_args: The arguments to pass to the weighting model.
             num_results: The maximum number of results to return per query.
+            include_fields: The metadata fields to return for each search result.
             threads: The number of threads to use during retrieval.
+            verbose: Whether to progress information during retrieval
 
         Returns:
-            A retriever object for this index.
+            A retriever transformer for this index.
         """
-        return pt.terrier.Retriever(self.index_obj(), controls, properties, metadata, num_results, wmodel)
+        return TerrierRetriever(self, model, model_args, include_fields=include_fields, num_results=num_results, threads=threads, verbose=verbose)
 
     def bm25(
         self,
@@ -47,7 +60,9 @@ class TerrierIndex(pt.Artifact):
         k1: float = 1.2,
         b: float = 0.75,
         num_results: int = 1000,
+        include_fields: Optional[List[str]] = None,
         threads: int = 1,
+        verbose: bool = False,
     ) -> pt.Transformer:
         """Creates a BM25 retriever for this index.
 
@@ -55,35 +70,59 @@ class TerrierIndex(pt.Artifact):
             k1: BM25's ``k1`` parameter, which controls TF saturation.
             b: BM25's ``b`` parameter, which controls the length penalty.
             num_results: The maximum number of results to return per query.
+            include_fields: The metadata fields to return for each search result.
             threads: The number of threads to use during retrieval.
+            verbose: Whether to progress information during retrieval
         """
         return self.retriever(
-            wmodel='BM25',
-            controls={'bm25.k_1': k1, 'bm25.b': b},
+            Model.bm25,
+            {'bm25.k1': k1, 'bm25.b': b},
             num_results=num_results,
-            threads=threads
+            include_fields=include_fields,
+            threads=threads,
+            verbose=verbose,
         )
 
     def dph(
         self,
         *,
         num_results: int = 1000,
+        include_fields: Optional[List[str]] = None,
         threads: int = 1,
+        verbose: bool = False,
     ) -> pt.Transformer:
         """Creates a DPH retriever for this index.
 
         Args:
             num_results: The maximum number of results to return per query.
+            include_fields: The metadata fields to return for each search result.
             threads: The number of threads to use during retrieval.
+            verbose: Whether to progress information during retrieval
         """
         return self.retriever(
-            wmodel='DPH',
+            Model.dph,
             num_results=num_results,
-            threads=threads
+            include_fields=include_fields,
+            threads=threads,
+            verbose=verbose,
         )
 
+    def text_loader(self,
+        index,
+        fields: Union[List[str], str, Literal['*']] = '*',
+        *,
+        verbose=False
+    ):
+        """Creates a text loader transformer for this index.
+
+        Args:
+            fields: The fields to load from the index. If '*', all fields will be loaded.
+            verbose: Whether to print progress information.
+        """
+        return pt.terrier.TerrierTextLoader(self, fields, verbose=verbose)
+
     def __repr__(self):
-        return f'TerrierIndex({self.path!r})'
+        return f'TerrierIndex({str(self.path)!r})'
 
     @pt.java.required
     def index_ref(self):
@@ -97,3 +136,111 @@ class TerrierIndex(pt.Artifact):
         if self._index_obj is None:
             self._index_obj = pt.terrier.IndexFactory.of(self.index_ref())
         return self._index_obj
+
+    @classmethod
+    @pt.java.required
+    def coerce(cls, index_like: Union[str, Path, 'TerrierIndex']) -> 'TerrierIndex':
+        """Attempts to build a :class:`TerrierIndex`` from the given object.
+        
+        ``index_like`` can be either: (bulleted list below):
+        - ``str`` or ``Path``: loads the index at the provided path
+        - ``pyterrier.terrier.J.IndexRef``: TODO: How to handle?
+        - ``pyterrier.terrier.J.Index``: TODO: How to handle?
+        - ``TerrierIndex``: returns itself
+        """
+        if isinstance(index_like, TerrierIndex):
+            return index_like
+        if isinstance(index_like, (str, Path)):
+            return TerrierIndex(index_like)
+        if isinstance(index_like, pt.terrier.J.IndexRef):
+            return TerrierIndex(_IN_MEM_PATH_PLACEHOLDER, _index_ref=index_like)
+        if isinstance(index_like, pt.terrier.J.Index):
+            return TerrierIndex(_IN_MEM_PATH_PLACEHOLDER, _index_obj=index_like)
+        raise RuntimeError(f'Could not coerce {index_like!r} into a TerrierIndex')
+
+
+class TerrierRetriever(pt.Transformer):
+    """A Terrier retriever.
+
+    This is a simplified (but less powerful) wrapper around ``pt.terrier.Retriever``.
+    """
+
+    def __init__(self,
+        index: TerrierIndex,
+        model: Union[Model, str],
+        model_args: Optional[Dict[str, Any]] = None,
+        *,
+        num_results: int = 1000,
+        include_fields: Optional[List[str]] = None,
+        threads: int = 1,
+        verbose: bool = False,
+    ):
+        """Initialises a TerrierRetriever."""
+        self._index = index
+        self.model = Model(model)
+        self.model_args = model_args
+        self.num_results = num_results
+        self.include_fields = include_fields
+        self.threads = threads
+        self.verbose = verbose
+
+    def transform(self, queries):
+        """Retrieves documents for the given queries."""
+        include_fields = self.include_fields
+        if include_fields is None:
+            include_fields = ['docno']
+        else:
+            if 'docno' not in include_fields:
+                include_fields = ['docno'] + include_fields
+        retr = pt.terrier.Retriever(
+            self._index.index_obj(),
+            controls=_map_controls(self.model_args),
+            properties=_map_properties(self.model_args),
+            metadata=include_fields,
+            num_results=self.num_results,
+            wmodel=_map_wmodel(self.model),
+            threads=self.threads,
+            verbose=self.verbose,
+        )
+        return retr(queries)
+
+    def __repr__(self):
+        return (f'TerrierRetriever({self.index!r}, {self.model!r}, {self.model_args!r}, '
+                f'num_results={self.num_results!r}, include_fields={self.include_fields!r}, threads={self.threads!r}, '
+                f'verbose={self.verbose!r})')
+
+    def fuse_rank_cutoff(self, k: int) -> Optional[pt.Transformer]:
+        if self.num_results > k:
+            return TerrierRetriever(self.index, self.model, self.model_args, num_results=k, include_fields=self.include_fields, threads=self.threads, verbose=self.verbose)
+
+
+_WMODEL_MAP = {
+    Model.bm25: 'BM25',
+    Model.dph: 'DPH',
+}
+def _map_wmodel(model):
+    return _WMODEL_MAP[model]
+
+
+_CONTROL_MAP = {
+    'bm25.k1': 'bm25.k_1',
+    'bm25.b': 'bm25.b',
+}
+def _map_controls(model_args):
+    return {
+        _CONTROL_MAP[k]: v
+        for k, v in model_args.items()
+        if k in _CONTROL_MAP
+    }
+
+
+_PROPERTY_MAP = {
+    'bm25.k1': 'bm25.k_1',
+    'bm25.b': 'bm25.b',
+}
+def _map_properties(model_args):
+    return {
+        _PROPERTY_MAP[k]: v
+        for k, v in model_args.items()
+        if k in _PROPERTY_MAP
+    }
