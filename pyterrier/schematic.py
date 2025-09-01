a0eb76c7e2622d588ca0193f3fcfc51942ad34bb
Sean MacAvaney <sean.macavaney@gmail.com>
2025-09-01 14:06:30 +0100

    rendering issue warning

--- pyterrier/schematic.py
+++ pyterrier/schematic.py
@@ -151,7 +151,7 @@ def draw_html_schematic(schematic: dict, *, outer_class: Optional[str] = None) -
     uid = str(uuid.uuid4())
     css, js = _get_schematic_css_js(f'id-{uid}')
     return f'''
-    <div id="id-{uid}" class="{outer_class or ''}">
+    <div id="id-{uid}" class="{outer_class or ''}" style="display: none;">
         <style>{css}</style>
         {_draw_html_schematic(schematic)}
         <div class="pts-infobox">
@@ -160,6 +160,9 @@ def draw_html_schematic(schematic: dict, *, outer_class: Optional[str] = None) -
             <div class="pts-infobox-hint"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  style="width: 12px; height: 12px; vertical-align: -2px;"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z" /><path d="M12 2a1 1 0 0 1 .993 .883l.007 .117v1a1 1 0 0 1 -1.993 .117l-.007 -.117v-1a1 1 0 0 1 1 -1z" /><path d="M21 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z" /><path d="M4.893 4.893a1 1 0 0 1 1.32 -.083l.094 .083l.7 .7a1 1 0 0 1 -1.32 1.497l-.094 -.083l-.7 -.7a1 1 0 0 1 0 -1.414z" /><path d="M17.693 4.893a1 1 0 0 1 1.497 1.32l-.083 .094l-.7 .7a1 1 0 0 1 -1.497 -1.32l.083 -.094l.7 -.7z" /><path d="M14 18a1 1 0 0 1 1 1a3 3 0 0 1 -6 0a1 1 0 0 1 .883 -.993l.117 -.007h4z" /><path d="M12 6a6 6 0 0 1 3.6 10.8a1 1 0 0 1 -.471 .192l-.129 .008h-6a1 1 0 0 1 -.6 -.2a6 6 0 0 1 3.6 -10.8z" /></svg> Click to explore!</div>
         </div>
         <script>{js}</script>
+    </div>
+    <div id="id-{uid}-pts-rendering-issue">
+        Rendering isssue. Try running the cell again.
     </div>
     '''
 
